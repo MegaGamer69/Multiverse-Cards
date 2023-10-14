a8f692bdd55100c1652e7413b7cec26b34a1e7ed
megagamerotario@gmail.com <megagamerotario@gmail.com>
2023-10-14 19:05:46 -0300

    lol

---
+++ source/Main.cpp
@@ -0,0 +1,17 @@
+/**
+ *  code by MegaGamer69(me)
+**/
+
+// include the SFML basics
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+
+class Game {
+public:
+    sf::RenderWindow((720, 1280), "Multiverse Cards");
+}
+
+int main() {
+    return 0;
+}
