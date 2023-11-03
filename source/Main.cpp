a7bbf9d369994dfec2cc37946578cc5035a77379
megagamerotario@gmail.com <megagamerotario@gmail.com>
2023-11-03 13:02:49 -0300

    é na sola da bota

--- source/Main.cpp
+++ source/Main.cpp
@@ -11,7 +11,7 @@
 // include the STD lib
 #include <string>
 #include <iostream>
-#include <list>
+#include <vector>
 
 using namespace sf;
 
@@ -48,8 +48,19 @@ class Trainer {
   public:
     Trainer(const std::string Name, const std::string Texture, int Health, int Damage, float AtkSpeed) :
     TrainerName(Name), TexturePath(Texture), TrainerHealth(Health), TrainerDamage(Damage), TrainerAtkSpeed(AtkSpeed) {
-        // nothing to see here :(
+        
     }
+    
+    void ReceiveDamage(int Damage) {
+        TrainerHealth -= Damage;
+    }
+    
+    void CheckHP() {
+        if(TrainerHealth <= 0) {
+            std::cout << "Trainer has been defeated" << std::endl;
+        }
+    }
+    
   private:
     const std::string TrainerName;
     const std::string TexturePath;
@@ -57,22 +68,6 @@ class Trainer {
     int TrainerDamage;
     float TrainerAtkSpeed;
 };
-
-void CardsList() {
-    // all the cards and trainers
-    // spell cards are not vulnerable
-    Trainer T1("Pirate", "../assets/images/trainer/trainer1.png", 720, 111, 1.8f);
-    Trainer T2("Thief", "../assets/images/trainer/trainer2.png", 700, 120, 1.5f);
-    Card C1("Reaper", "../assets/images/card/card1.png", 444, 96, 1.2f, 2, "Troop", false, false, 70, 4, false);
-    Card C2("Indians", "../assets/images/cards/card2.png", 120, 50, 1, 2.1f, "Troop", false, true, 68, 2, false);
-    Card C3("Cannon", "../assets/images/cards/card3.png", 390, 70, 1.1f, 0, "Building", false, false, 600, 3, false);
-    Card C4("Pelican", "../assets/images/cards/card4.png", 600, 96, 2, 1.8f, "Troop", true, false, 100, 5, true);
-    Card C5("Fireball", "../assets/images/cards/card5.png", 0, 150, 1.5f, 0, "Spell", true, true, 0, 4, false);
-    Card C6("Eletric Tower", "../assets/images/cards/card6.png", 300, 70, 1.1f, 0, "Building", false, true, 600, 4, false);
-    Card C7("Meteore", "../assets/images/cards/card7.png", 0, 300, 5, 0, "Spell", true, true, 0, 6, false);
-    Card C8("Titan", "../assets/images/cards/card8.png", 1200, 90, 3, 1, "Troop", false, false, 1024, 8, true);
-    Card C9("Samurai", "../assets/images/cards/card9.png", 960, 111, 1.7f, 2, "Troop", false, false, 814, 7, false);
-}
 
 class Game {
   public:
@@ -95,21 +90,36 @@ class Game {
     }
   private:
     RenderWindow Display;
-    
-    std::list<CardsList> List;
-    
-    List.insert(T1);
-    List.insert(C1);
-    List.insert(C2);
-    List.insert(C3);
-    List.insert(C4);
-    List.insert(C5);
-    List.insert(C6);
 };
 
 int main() {
     Game MainGame;
     MainGame.Run();
+    
+    // all the cards and trainers in game
+    Trainer T1("Pirate", "../assets/images/trainer/trainer1.png", 720, 111, 1.8f);
+    Trainer T2("Thief", "../assets/images/trainer/trainer2.png", 700, 120, 1.5f);
+    Card C1("Reaper", "../assets/images/card/card1.png", 444, 96, 1.2f, 2, "Troop", false, false, 70, 4, false);
+    Card C2("Indians", "../assets/images/cards/card2.png", 120, 50, 1, 2.1f, "Troop", false, true, 68, 2, false);
+    Card C3("Cannon", "../assets/images/cards/card3.png", 390, 70, 1.1f, 0, "Building", false, false, 600, 3, false);
+    Card C4("Pelican", "../assets/images/cards/card4.png", 600, 96, 2, 1.8f, "Troop", true, false, 100, 5, true);
+    Card C5("Fireball", "../assets/images/cards/card5.png", 0, 150, 1.5f, 0, "Spell", true, true, 0, 4, false);
+    Card C6("Eletric Tower", "../assets/images/cards/card6.png", 300, 70, 1.1f, 0, "Building", false, true, 600, 4, false);
+    Card C7("Meteore", "../assets/images/cards/card7.png", 0, 300, 5, 0, "Spell", true, true, 0, 6, false);
+    Card C8("Titan", "../assets/images/cards/card8.png", 1200, 90, 3, 1, "Troop", false, false, 1024, 8, true);
+    Card C9("Samurai", "../assets/images/cards/card9.png", 960, 111, 1.7f, 2, "Troop", false, false, 814, 7, false);
+    
+    // create a deck
+    std::vector<Card> Deck;
+    
+    // add the cards in deck
+    Deck.push_back(T1);
+    Deck.push_back(C1);
+    Deck.push_back(C2);
+    Deck.push_back(C3);
+    Deck.push_back(C4);
+    Deck.push_back(C5);
+    Deck.push_back(C6);
     
     return 0;
 }
