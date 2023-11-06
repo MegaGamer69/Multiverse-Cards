59cca98fbcef8cb60d942a17f08b2305bd5c007b
megagamerotario@gmail.com <megagamerotario@gmail.com>
2023-11-06 20:24:33 -0300

    update Main.cpp

--- source/Main.cpp
+++ source/Main.cpp
@@ -39,14 +39,24 @@ class Energy {
 
 class Card {
   public:
-    Card(const std::string Name, const std::string Texture, int Health, int Damage, float AttackSpeed, float MoveSpeed, const std::string Type, bool Flying, bool AtkInAirAlso, int Mass, int Cost, bool AtkOnlyBuild, float AtkRange, int HealEffect) :
-    CardName(Name), CardTexture(Texture), CardHealth(Health), CardDamage(Damage), CardAttackSpeed(AttackSpeed), CardMoveSpeed(MoveSpeed), CardType(Type), FlyingCard(Flying), CardAtkAirAlso(AtkInAirAlso), CardMass(Mass), CardCost(Cost), AtkOnlyBuildings(AtkOnlyBuild), CardAtkRange(AtkRange), CardHealEffect(HealEffect) {
-        if(!TextureLOL.loadFromFile(CardTexture)) {
-            std::cout << "A" << std::endl;
-        } else {
-            std::cout << "B" << std::endl;
-        }
-    };
+    Card();
+    
+    void setStatus(const std::string Name, const std::string Texture, int Health, int Damage, float AttackSpeed, float MoveSpeed, const std::string Type, bool Flying, bool AtkAirAlso, int Mass, int Cost, bool AtkOnlyBuild, float AtkRange, int HealEffect) {
+        CardName = Name;
+        CardTexture = Texture;
+        CardHealth = Health;
+        CardDamage = Damage;
+        CardAttackSpeed = AttackSpeed;
+        CardMoveSpeed = MoveSpeed;
+        CardType = Type;
+        FlyingCard = Flying;
+        CarxAtkAirAlso = AtkAirAlso;
+        CardMass = Mass;
+        CardCost = Cost;
+        AtkOnlyBuildings = AtkOnlyBuild;
+        CardAtkRange = AtkRange;
+        CardHealEffect = HealEffect;
+    }
     
     void Deploy() {
         EnergyInstancie.UseEnergy(CardCost);
@@ -76,10 +86,16 @@ class Card {
 // trainers is the most important on game, if your trainer has been defeated, game over
 class Trainer {
   public:
-    Trainer(const std::string Name, const std::string Texture, int Health, int Damage, float AtkSpeed, float AtkRange) :
-    TrainerName(Name), TexturePath(Texture), TrainerHealth(Health), TrainerDamage(Damage), TrainerAtkSpeed(AtkSpeed), TrainerAtkRange(AtkRange) {
-        
-    };
+    Trainer();
+    
+    void setStatus(const std::string Name, const std::string Texture, int Health, int Damage, float AtkSpeed, float AtkRange) {
+        TrainerName = Name;
+        TexturePath = Texture;
+        TrainerHealth = Health;
+        TrainerDamage = Damage;
+        TrainerAtkSpeed = AtkSpeed;
+        TrainerAtkRange = AtkRange;
+    }
     
     void ReceiveDamage(int Damage) {
         TrainerHealth -= Damage;
@@ -102,21 +118,36 @@ class Trainer {
 
 int main() {
     // all the cards and trainers in game
-    Trainer T1("Pirate", "../assets/images/trainer/trainer1.png", 720, 111, 1.8f, 1.5f);
-    Trainer T2("Thief", "../assets/images/trainer/trainer2.png", 700, 120, 1.5f, 6.5f);
-    Trainer T3("Master", "../assets/images/trainer/trainer3.png", 880, 80, 1.7f, 4);
-    Card C1("Reaper", "../assets/images/card/card1.png", 444, 96, 1.2f, 2, "Troop", false, false, 70, 4, false, 1.25f, 0);
-    Card C2("Indians", "../assets/images/cards/card2.png", 120, 50, 1, 2.1f, "Troop", false, true, 68, 2, false, 4.5f, 0);
-    Card C3("Cannon", "../assets/images/cards/card3.png", 390, 70, 1.1f, 0, "Building", false, false, 600, 3, false, 4.75f, 0);
-    Card C4("Pelican", "../assets/images/cards/card4.png", 600, 96, 2, 1.8f, "Troop", true, false, 100, 5, true, 1, 0);
-    Card C5("Fireball", "../assets/images/cards/card5.png", 0, 150, 1.5f, 0, "Spell", true, true, 0, 4, false, 2.5f, 0);
-    Card C6("Eletric Tower", "../assets/images/cards/card6.png", 300, 70, 1.1f, 0, "Building", false, true, 600, 4, false, 5.5f, 0);
-    Card C7("Meteore", "../assets/images/cards/card7.png", 0, 300, 5, 0, "Spell", true, true, 0, 6, false, 4, 0);
-    Card C8("Titan", "../assets/images/cards/card8.png", 1200, 90, 3, 1, "Troop", false, false, 1024, 8, true, 1, 0);
-    Card C9("Samurai", "../assets/images/cards/card9.png", 960, 111, 1.7f, 2, "Troop", false, false, 128, 7, false, 1.25f, 0);
-    Card C10("Bomb Man", "../assets/images/cards/card10.png", 222, 97, 1, 4, "Troop", false, false, 60, 3, true, 0.75f, 0);
-    Card C11("Terrorist", "../assets/images/cards/card11.png", 660, 74, 1.1f, 2, "Troop", false, true, 70, 4, false, 1, 0);
-    Card C12("Heal", "../assets/images/cards/card12.png", 0, 0, 1, 0, "Spell", false, false, 0, 3, false, 4, 50);
+    Trainer T1;
+    T1.setStatus("Pirate", "../assets/images/trainer/trainer1.png", 720, 111, 1.8f, 1.5f);
+    Trainer T2;
+    T2.setStatus("Thief", "../assets/images/trainer/trainer2.png", 700, 120, 1.5f, 6.5f);
+    Trainer T3;
+    T3.setStatus("Master", "../assets/images/trainer/trainer3.png", 880, 80, 1.7f, 4);
+    Card C1;
+    C1.setStatus("Reaper", "../assets/images/card/card1.png", 444, 96, 1.2f, 2, "Troop", false, false, 70, 4, false, 1.25f, 0);
+    Card C2;
+    C2.setStatus("Indians", "../assets/images/cards/card2.png", 120, 50, 1, 2.1f, "Troop", false, true, 68, 2, false, 4.5f, 0);
+    Card C3;
+    C3.setStatus("Cannon", "../assets/images/cards/card3.png", 390, 70, 1.1f, 0, "Building", false, false, 600, 3, false, 4.75f, 0);
+    Card C4;
+    C4.setStatus("Pelican", "../assets/images/cards/card4.png", 600, 96, 2, 1.8f, "Troop", true, false, 100, 5, true, 1, 0);
+    Card C5;
+    C5.setStatus("Fireball", "../assets/images/cards/card5.png", 0, 150, 1.5f, 0, "Spell", true, true, 0, 4, false, 2.5f, 0);
+    Card C6;
+    C6.setStatus("Eletric Tower", "../assets/images/cards/card6.png", 300, 70, 1.1f, 0, "Building", false, true, 600, 4, false, 5.5f, 0);
+    Card C7;
+    C7.setStatus("Meteore", "../assets/images/cards/card7.png", 0, 300, 5, 0, "Spell", true, true, 0, 6, false, 4, 0);
+    Card C8;
+    C8.setStatus("Titan", "../assets/images/cards/card8.png", 1200, 90, 3, 1, "Troop", false, false, 1024, 8, true, 1, 0);
+    Card C9;
+    C9.setStatus("Samurai", "../assets/images/cards/card9.png", 960, 111, 1.7f, 2, "Troop", false, false, 128, 7, false, 1.25f, 0);
+    Card C10;
+    C10.setStatus("Bomb Man", "../assets/images/cards/card10.png", 222, 97, 1, 4, "Troop", false, false, 60, 3, true, 0.75f, 0);
+    Card C11;
+    C11.setStatus("Terrorist", "../assets/images/cards/card11.png", 660, 74, 1.1f, 2, "Troop", false, true, 70, 4, false, 1, 0);
+    Card C12;
+    C12.setStatus("Heal", "../assets/images/cards/card12.png", 0, 0, 1, 0, "Spell", false, false, 0, 3, false, 4, 50);
     
     sf::Window Display;
     Display.create(sf::VideoMode(720, 1280, 32), "Multiverse Cards");
