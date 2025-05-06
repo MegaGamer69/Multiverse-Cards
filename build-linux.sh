bfa1dc2d41d943180466ad6a457f46fef16a0f76
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2025-05-06 18:10:53 -0300

    Add files via upload

---
+++ build-linux.sh
@@ -0,0 +1,1 @@
+gcc ./source/c/mcards.c -o mcards -I./include -L./libs/linux -lcsfml-graphics -lcsfml-window -lcsfml-system -O2
---
+++ build-windows.sh
@@ -0,0 +1,1 @@
+x86_64-w64-mingw32-gcc ./source/c/mcards.c -o mcards.exe -I./include -L./libs/windows -lcsfml-graphics-2 -lcsfml-window-2 -lcsfml-system-2 -O2
