e880a8d2b099366f32cf3e818b0f2068ef219f2f
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2025-05-07 22:11:18 -0300

    Update build-both.sh

--- build-both.sh
+++ build-both.sh
@@ -2,4 +2,4 @@ echo "Build to Linux"
 gcc ./source/c/mcards.c -o distr/mcards -I./include -lcsfml-graphics -lcsfml-window -lcsfml-system -O2
 
 echo "Build to Windows"
-x86_64-w64-mingw32-gcc ./source/c/mcards.c -o distr/mcards.exe -I./include -lcsfml-graphics -lcsfml-window -lcsfml-system -O2
+gcc ./source/c/mcards.c -o distr/mcards.exe -I./include -lcsfml-graphics -lcsfml-window -lcsfml-system -O2
