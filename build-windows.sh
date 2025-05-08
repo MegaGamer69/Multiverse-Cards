0d6307692d8d5f68f9536a893b16914be37c09f3
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2025-05-07 22:16:18 -0300

    Update build-windows.sh

--- build-windows.sh
+++ build-windows.sh
@@ -1,1 +1,2 @@
-x86_64-w64-mingw32-gcc ./source/c/mcards.c -o mcards.exe -I./include -L./libs/windows -lcsfml-graphics-2 -lcsfml-window-2 -lcsfml-system-2 -O2
+echo "Build to Windows"
+gcc ./source/c/mcards.c -o distr/mcards.exe -I./include -lcsfml-graphics -lcsfml-window -lcsfml-system -O2
