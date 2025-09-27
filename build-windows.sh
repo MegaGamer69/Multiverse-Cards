786bd3cbbff1d57584534f28163e155779451627
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2025-09-27 19:20:11 -0300

    Delete build-windows.sh

--- build-windows.sh
+++
@@ -1,9 +0,0 @@
-if [ -e "./distr/bin/mcards.exe" ]; then
-	echo "Limpando o diretório de buildagem"
-	
-	rm -rf "./distr/bin/mcards.exe"
-fi
-
-echo "Buildando para Windows"
-
-gcc ./source/c/main.c -o "./distr/bin/mcards.exe" -I./include -lcsfml-graphics -lcsfml-window -lcsfml-system -lzip -O2
