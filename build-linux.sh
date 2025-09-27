d2bc465045bed76271ade768c4aea14ae1669872
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2025-09-27 19:20:20 -0300

    Delete build-linux.sh

--- build-linux.sh
+++
@@ -1,9 +0,0 @@
-if [ -e "./distr/bin/mcards" ]; then
-	echo "Limpando o diretório de buildagem"
-	
-	rm -rf "./distr/bin/mcards"
-fi
-
-echo "Buildando para Linux"
-
-gcc ./source/c/main.c -o "./distr/bin/mcards" -I./include -lcsfml-graphics -lcsfml-window -lcsfml-system -lzip -O2
