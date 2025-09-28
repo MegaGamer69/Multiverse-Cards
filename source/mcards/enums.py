35b89f030cbf0f764cad6fbb00c04018dc1e3efb
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2025-09-28 19:23:55 -0300

    Update enums.py

--- source/mcards/enums.py
+++ source/mcards/enums.py
@@ -1,6 +1,28 @@
 from enum import Enum
 
-class TargetTypes(Enum):
+class CardNameVariations(Enum):
+	GRAVEDIGGER = {
+		"en_us": "Gravedigger",
+		"pt_br": "Coveirinha",
+		"es_sp": "Sepulturero",
+		"de_de": "Totengräber",
+	}
+
+class CardType(Enum):
+	TROOP = "Troop"
+	SPELL = "Spell"
+	BUILDING = "Building"
+
+class CardRarities(Enum):
+	COMMON = "Common"
+	RARE = "Rare"
+	EPIC = "Epic"
+
+class MovementType(Enum):
+	GROUND = "Ground"
+	AIR = "Air"
+
+class TargetType(Enum):
 	ONLY_GROUND_BUILDING_AND_HEROES = "Only ground, building and heroes"
 	ONLY_AIR_BUILDING_AND_HEROES = "Only air, building and heroes"
 	ANY = "Any"
