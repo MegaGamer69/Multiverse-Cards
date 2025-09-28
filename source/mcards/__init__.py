35c9f774eedc59315e550fbf4da6b9ce1cba2c48
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2025-09-28 19:23:15 -0300

    Update __init__.py

--- source/mcards/__init__.py
+++ source/mcards/__init__.py
@@ -1,8 +1,12 @@
-__all__ = ["engine"]
+__all__ = ["engine", "enums", "gvars"]
 
 from . import engine
+from . import enums
+from . import gvars
 
-from .gameplay.core import units
+from .gameplay.core import units, cards
+from .gameplay import player
+from .gameplay import game_state
 
 __version__ = "1.0.0"
 
