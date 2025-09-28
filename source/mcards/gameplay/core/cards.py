ba1aa364219dff1b5bb8950daa519f0c0609fb0c
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2025-09-28 19:25:40 -0300

    Update cards.py

--- source/mcards/gameplay/core/cards.py
+++ source/mcards/gameplay/core/cards.py
@@ -2,21 +2,25 @@
 
 import pygame
 
-class Cards(pygame.sprite.Sprite):
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+	from mcards.gameplay.player import Player
+
+class Card(pygame.sprite.Sprite):
 	"""
 	The base game in the game.
 	Contains the basic system to summon units and some other stuffs.
 	"""
 	
-	def __init__(self, key_id: str, name: str, level: int, rarity: str,
-				cost: int, summons: list[str], blue_team: bool):
+	def __init__(self, name: dict[str, str], level: int, rarity: str,
+				cost: int, summons: dict[str, int], blue_team: bool):
 		"""
 		Initialize the card system.
 		"""
 		
 		super().__init__()
 		
-		self.__key_id = key_id
 		self.__name = name
 		self.__level = level
 		self.__rarity = rarity
@@ -29,7 +33,7 @@ class Cards(pygame.sprite.Sprite):
 		
 		self.__position = pygame.math.Vector2(0, 0)
 	
-	def update(self, user: Player):
+	def update(self, user: 'Player'):
 		"""
 		Updates the ard's sprite state.
 		"""
