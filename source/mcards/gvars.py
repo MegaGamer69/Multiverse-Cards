563c0d743b2855eb786ffabafa2607699fbf727a
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2025-09-28 19:24:06 -0300

    Add files via upload

---
+++ source/mcards/gvars.py
@@ -0,0 +1,11 @@
+# Module path: mcards.gvars
+
+"""
+Determines if the OS is 32 Bits (4 GB R.A.M. optimizations).
+"""
+global IS_32_BITS
+
+"""
+The window size (for non-mobile systems).
+"""
+global WINDOW_SIZE
